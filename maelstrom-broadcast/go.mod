569ef75961fa393f088421f2b019b1cdb750df4c
swapnil159 <swapnilmahajan159@gmail.com>
2024-12-06 13:37:45 +0530

    Gossip Glomers challenge 3c

--- maelstrom-broadcast/go.mod
+++ maelstrom-broadcast/go.mod
@@ -2,4 +2,7 @@ module maelstrom-broadcast
 
 go 1.23.3
 
-require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20240813160128-8b9e94c75e59 // direct
+require (
+	github.com/jasonlvhit/gocron v0.0.1
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20240813160128-8b9e94c75e59 // direct
+)
--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -7,6 +7,7 @@ import (
 	"slices"
 	"sync"
 
+	gocron "github.com/jasonlvhit/gocron"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
@@ -14,23 +15,32 @@ type server struct {
 	node       *maelstrom.Node
 	values     map[int]bool
 	neighbours topology
+	buffer     map[task]int
 
 	valuesMu sync.RWMutex
 	topoMu   sync.RWMutex
+	buffMu   sync.RWMutex
 }
 
 type topology struct {
 	topo []string
 }
 
+type task struct {
+	receiver string
+	id       int
+}
+
 func main() {
 	n := maelstrom.NewNode()
-	s := &server{node: n, values: make(map[int]bool)}
+	s := &server{node: n, values: make(map[int]bool), buffer: make(map[task]int)}
 
 	n.Handle("broadcast", s.broadcast)
 	n.Handle("read", s.read)
 	n.Handle("topology", s.topology)
 	n.Handle("broadcast_ok", s.recvResp)
+
+	gocron.Every(10).Second().Do(s.sendToNeighbour)
 
 	if err := n.Run(); err != nil {
 		log.Printf("Error is %s", err)
@@ -50,7 +60,7 @@ func (s *server) broadcast(msg maelstrom.Message) error {
 	s.valuesMu.Lock()
 	var val = int(body["message"].(float64))
 	if !s.values[val] {
-		s.sendToNeighbour(val, msg.Src)
+		s.updateBuffer(val, msg.Src, int(body["msg_id"].(float64)))
 	}
 	s.values[val] = true
 	defer s.valuesMu.Unlock()
@@ -107,30 +117,46 @@ func (s *server) topology(msg maelstrom.Message) error {
 	return s.node.Reply(msg, body)
 }
 
-func (s *server) sendToNeighbour(val int, sender string) {
+func (s *server) updateBuffer(val int, sender string, msgId int) {
 	s.topoMu.RLock()
 	nbor := s.neighbours.topo
-	log.Printf("Neighbours are %s", nbor)
 	defer s.topoMu.RUnlock()
 
 	for _, n := range nbor {
 		if sender == n {
 			continue
 		}
-		err := s.node.Send(
-			n,
-			map[string]any{
-				"type":    "broadcast",
-				"message": float64(val),
-			},
-		)
-		if err != nil {
-			log.Printf("Error is %s", err)
-			log.Fatal(err)
-		}
+		s.buffMu.Lock()
+		t := task{receiver: n, id: msgId}
+		s.buffer[t] = val
+		defer s.buffMu.Unlock()
 	}
 }
 
 func (s *server) recvResp(msg maelstrom.Message) error {
+	// Unmarshal the message body as an loosely-typed map.
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	var val = int(body["message"].(float64))
+	t := task{receiver: msg.Src, id: val}
+
+	s.buffMu.Lock()
+	delete(s.buffer, t)
+	defer s.buffMu.Unlock()
 	return nil
+}
+
+func (s *server) sendToNeighbour() {
+	s.buffMu.RLock()
+	for key, val := range s.buffer {
+		s.node.Send(key.receiver, map[string]any{
+			"type":    "broadcast",
+			"message": float64(val),
+			"msg_id":  key.id,
+		})
+	}
+	defer s.buffMu.RUnlock()
 }
